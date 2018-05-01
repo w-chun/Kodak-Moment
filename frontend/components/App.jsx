bd3f282bf34428400e54c06412692e3154405bb0
Wilson <wchun108@gmail.com>
2018-04-30 18:15:50 -0700

    Setup ProtectedRoute

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import HomeContainer from './home/home_container';
 import SessionFormContainer from './session_form/session_form_container';
 import { Route } from 'react-router-dom';
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
     <h1>Kodak Moment</h1>
-    <Route exact path='/' component={HomeContainer} />
+    <Route exact path='/' component={SessionFormContainer} />
+    <ProtectedRoute path='/posts' component={HomeContainer} />
     <AuthRoute path='/login' component={SessionFormContainer} />
     <AuthRoute path='/signup' component={SessionFormContainer} />
   </div>
