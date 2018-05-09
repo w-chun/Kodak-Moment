4007782558a093a3faeb7d04d82cabbfe03a0939
Wilson <wchun108@gmail.com>
2018-05-09 14:48:15 -0700

    Style greeting container

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -9,12 +9,14 @@ const sessionLinks = () => (
 );
 
 const personalGreeting = (currentUser, logout) => (
-  <header>
-    <div><Link to='/upload'>Upload</Link></div>
-    <div>
-      <div><img src={currentUser.img_url}></img>Welcome {currentUser.username}!</div>
+  <header className='header'>
+    <div className='header-home-link'><Link to='/posts'><i class="fas fa-camera-retro"></i><div className='header-title'>Kodak Moment</div></Link></div>
+    <div className='upload-link'><Link to='/upload'><i class="fas fa-upload"></i>Upload</Link></div>
+    <div className='nav-links'>
+      <div><i class="far fa-compass"></i></div>
+      <div><i class="far fa-user"></i></div>
+      <div><i class="fas fa-sign-out-alt" onClick={logout}></i></div>
     </div>
-    <div><button onClick={logout}>Log Out</button></div>
   </header>
 );
 
