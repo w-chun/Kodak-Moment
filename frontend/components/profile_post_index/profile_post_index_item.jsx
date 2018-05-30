d6fd29e55ecc2168b581e4d2fcbc3f34fdb1b7c1
Wilson <wchun108@gmail.com>
2018-05-29 17:29:07 -0700

    Create Modal for profile posts

--- frontend/components/profile_post_index/profile_post_index_item.jsx
+++ frontend/components/profile_post_index/profile_post_index_item.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 export default class ProfilePostIndexItem extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      modalIsOpen: false
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  componentWillMount() {
+    Modal.setAppElement('body');
+  }
+
+  openModal() {
+    this.setState({modalIsOpen:true});
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen:false});
   }
 
   render() {
     const { post } = this.props;
     return (
       <div className='profile-post-wrapper'>
-        <img src={post.img_url} className='profile-post-index-image'></img>
+        <a onClick={this.openModal}><img src={post.img_url} className='profile-post-index-image'></img></a>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onClose={this.closeModal}
+          style={customStyles}
+          >
+        <div className='modal-close'><i className="fas fa-times" onClick={this.closeModal}></i></div>
+        </Modal>
       </div>
     );
   }
