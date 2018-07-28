a7839cade349e7d39f9fd6f249e85312737c23a2
Wilson <wchun108@gmail.com>
2018-07-27 20:03:58 -0700

    Create post for users not following anyone

--- frontend/components/post_index/post_index.jsx
+++ frontend/components/post_index/post_index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostIndexItem from './post_index_item';
+import { Link } from 'react-router-dom';
 
 export default class PostIndex extends React.Component {
   constructor(props){
@@ -14,7 +15,8 @@ export default class PostIndex extends React.Component {
 
   render() {
     let index;
-    if (this.props.user) {
+    const { user } = this.props;
+    if (this.props.user && this.props.posts.filter(post => this.props.followeesIds.includes(post.author_id)).length !== 0) {
       index = <ul className='post-index-container'>
                 {this.props.posts.sort(function (a,b) {
                   return (b.id - a.id);
@@ -28,6 +30,28 @@ export default class PostIndex extends React.Component {
                 ))
                 }
               </ul>;
+    } else if (user) {
+      index = <div className='find-people'>
+                <div className='find-people-post'>
+                  <div className='post-user'>
+                    <img className='post-user-image' src={user.img_url}/>
+                    <Link to={`/users/${user.id}`} className='post-profile-link'>
+                      {user.username}
+                    </Link>
+                  </div>
+                  <div className='no-followers'>
+                    Oops you're not following anyone right now. <br />
+                    <Link to='/discover' className='explore-link'>Explore for people and Kodaks here</Link>
+                  </div>
+                  <div className='fake-likes-comments'>
+                    <i className="far fa-heart"></i>
+                    <i className="far fa-comment"></i>
+                  </div>
+                  <div className='fake-post-caption'>
+                    <Link to={`/users/${user.id}`} className='caption-username'><b>{user.username}</b></Link> Go Follow some people!
+                  </div>
+                </div>
+              </div>;
     }
     return (
       <div>
